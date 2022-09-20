c96ec81b4d656df658f8c563cc6dc7b4908a1f35
tiberna <tiago.bernardo@gmail.com>
2022-09-20 21:15:08 +0000

    Fix configurations

--- src/Services/AssetsService/AssetsService.API/Models/Account.cs
+++ src/Services/AssetsService/AssetsService.API/Models/Account.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Tiberna.MyFinancePal.AssetsService.API.Models;
 
 public class Account
@@ -8,6 +6,8 @@ public class Account
     public string? Name { get; set; }
     public string? BankAccountId { get; set; }
     public bool IsActive { get; set; } = true;
+    public int AccountCurrencyCurrencyId { get; set; }
+    public Currency? AccountCurrency { get; set; }
     public string? Description { get; set; }
     public DateTime InitialBalanceDate { get; set; }
     public decimal InitialBalance { get; set; }
